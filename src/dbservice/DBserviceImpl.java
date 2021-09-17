4a874e9ba20d83e791164b8beccd60f001d107ef
juyeong <bn0406@naver.com>
2021-09-17 23:42:37 +0900

    .

--- src/dbservice/DBserviceImpl.java
+++ src/dbservice/DBserviceImpl.java
@@ -2,43 +2,15 @@ package dbservice;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 
 import dbcommon.DBCommon;
 import membership.MemberDTO;
-import memdto.MemDTO;
 
-public class DBserviceImpl implements DBService{
+public class DBServiceImpl implements DBService{
 	PreparedStatement ps;
 	ResultSet rs;
-	
-	@Override
-	public MemDTO loginCheck(String id) {
-		
-		String sql = "select * from MEMBER where id=?";
-		MemDTO dto = null;
-		try {
-			ps = DBCommon.con.prepareStatement(sql);
-			ps.setString(1, id);
-			rs = ps.executeQuery();
-			if(rs.next()) {
-				dto = new MemDTO();
-				dto.setId(rs.getString("id"));
-				dto.setPwd(rs.getString("pwd"));
-				dto.setName(rs.getString("name"));
-				dto.setPhone(rs.getString("phone"));
-			}
-			
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		return dto;
-	}
 	@Override
 	public int insertMember(MemberDTO dto) {
-		
 		String sql = "insert into member values(?,?,?,?)";
 		int result=0;
 		try {
@@ -55,8 +27,5 @@ public class DBserviceImpl implements DBService{
 		
 		return result;
 	}
-	
-	
 
 }
-
