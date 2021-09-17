3e171cf6dd619637c69f750fefcf9bdf5ca414b5
kwanghun <s5095940@naver.com>
2021-09-17 16:46:54 +0900

    연결만

--- src/miniProject/Controller.java
+++ src/miniProject/Controller.java
@@ -1,11 +1,15 @@
 package miniProject;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import dbcommon.DBCommon;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class Controller implements Initializable{
 		Parent root;
@@ -19,7 +23,22 @@ public class Controller implements Initializable{
 			
 		}
 		public void loginBut() {
-			System.out.println("로그인 버튼");
+			Stage stage = new Stage();
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("login.fxml"));
+			Parent root1=null;
+			try {
+				root1 = loader.load();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			Scene scene = new Scene(root1);
+			
+			LoginController ctl = loader.getController();
+			ctl.setRoot(root1);
+			
+			stage.setScene(scene);
+			stage.show();
 		}
 		public void seekBut() {
 			System.out.println("찾기 버튼");
@@ -31,22 +50,5 @@ public class Controller implements Initializable{
 			System.out.println("마스터 버튼");
 		}
 		
-		Parent root1;
-		   public void setRoot1(Parent root1) {
-		      this.root1 = root1;
-		      
-		   }
-		   
-		   public void bookSearch() {
-		      
-		   }
-		   public void borrow() {
-		      
-		   }
-		   public void recomBook() {
-		      
-		   }
-		   public void logOut() {
-		      
-		   }
+	
 }
