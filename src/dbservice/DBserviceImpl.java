1d17b0e964087c24c508b65e5b571c2e5d9b0afb
juyeong <bn0406@naver.com>
2021-09-17 20:35:34 +0900

    로그인 커밋 완료

--- src/dbservice/DBserviceImpl.java
+++ src/dbservice/DBserviceImpl.java
@@ -9,16 +9,16 @@ import membership.MemberDTO;
 import memdto.MemDTO;
 
 public class DBserviceImpl implements DBService{
-	
-	
+	PreparedStatement ps;
+	ResultSet rs;
 	public MemDTO loginCheck(String id) {
 		
 		String sql = "select * from MEMBER where id=?";
 		MemDTO dto = null;
 		try {
-			PreparedStatement ps = DBCommon.con.prepareStatement(sql);
+			ps = DBCommon.con.prepareStatement(sql);
 			ps.setString(1, id);
-			ResultSet rs = ps.executeQuery();
+			rs = ps.executeQuery();
 			if(rs.next()) {
 				dto = new MemDTO();
 				dto.setId(rs.getString("id"));
@@ -33,14 +33,26 @@ public class DBserviceImpl implements DBService{
 		}
 		
 		return dto;
-		
-		
 	}
-
 	@Override
 	public int insertMember(MemberDTO dto) {
-		// TODO Auto-generated method stub
-		return 0;
+		
+		String sql = "insert into member values(?,?,?,?)";
+		int result=0;
+		try {
+			ps=DBCommon.con.prepareStatement(sql);
+			ps.setString(1, dto.getId());
+			ps.setString(2, dto.getPwd());
+			ps.setString(3, dto.getName());
+			ps.setString(4, dto.getpNumber());
+			
+			result = ps.executeUpdate();
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		
+		return result;
 	}
 
 }
+
