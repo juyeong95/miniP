015ce3136f6cfbfc839869cc37b4e880780c96b8
juyeong <bn0406@naver.com>
2021-09-23 17:53:22 +0900

    9 23

--- src/miniProject/Controller.java
+++ src/miniProject/Controller.java
@@ -11,12 +11,16 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import membership.MembershipMain;
+import seek.SeekMain;
 import service.MyService;
 import service.MyServiceImpl;
 
 public class Controller implements Initializable{
 		Parent root;
+		MembershipMain msm;
 		MyService ms;
+		SeekMain sm;
 		public void setRoot(Parent root) {
 			this.root = root;
 			ms.setRoot(root);
@@ -26,16 +30,18 @@ public class Controller implements Initializable{
 		public void initialize(URL arg0, ResourceBundle arg1) {
 			ms = new MyServiceImpl();
 			DBCommon.setDBConnection(); //연결
+			msm = new MembershipMain();
+			sm = new SeekMain();
 			
 		}
 		public void loginBut() {
 			ms.login();
 		}
 		public void seekBut() {
-			System.out.println("찾기 버튼");
+			sm.OpenSeek();
 		}
 		public void memberBut() {
-			System.out.println("회원가입 버튼");
+			msm.OpenMembership();
 		}
 		public void masterBut() {
 			System.out.println("마스터 버튼");
