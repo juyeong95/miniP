f43b0e3e6a6264d4479ef01884c928b97885576f
Seo-on Sung <seoonsung@Seo-ons-MacBook-Pro.local>
2021-09-24 15:11:05 +0900

    masterlogin&closeStage

--- src/dbcommon/DBCommon.java
+++ src/dbcommon/DBCommon.java
@@ -3,12 +3,14 @@ package dbcommon;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
+import javafx.scene.Parent;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.stage.Stage;
 
 public class DBCommon {
 	public static Connection con;
+	static Parent root;
 	public static void setDBConnection() {
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
@@ -19,11 +21,14 @@ public class DBCommon {
 		}
 	}
 	
-	public static void getAlert(String msg) {
+	public static void getAlert(String msg) { //alert창 등록
 		Alert alert = new Alert(AlertType.INFORMATION);
 		alert.setContentText(msg);
 		alert.show();
 	}
 	
-	
+	public static void closeStage(Parent root) {	//작업창 닫기 
+		Stage stage = (Stage) root.getScene().getWindow();
+		stage.close();	
+	}
 }
--- src/master/MasterController.java
+++ src/master/MasterController.java
@@ -3,33 +3,52 @@ package master;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import dbcommon.DBCommon;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import master.login.MasterLoginMain;
+import memdto.MemDTO;
 
 public class MasterController implements Initializable{
 	MasterLoginMain mlm;
 	Parent root;
+	@FXML
+	private Button mlogin;
+	DBCommon db = new DBCommon();
+
 	public void setRoot(Parent root) {
 		this.root = root;
 		
-		
 	}
+	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		mlm = new MasterLoginMain();
-		
 	}
 	
-	
 	public void masterLogin() {  //마스터 login onAction
-		mlm.getFrom();
+		TextField id = (TextField) root.lookup("#masterId");//텍스트필드의 입력값 불러오기
+		PasswordField pwd = (PasswordField) root.lookup("#masterPwd");
+
+		if(id.getText().equals("master")) {	//마스터 로그인을 위한 아이디와 비밀번호를 미리 회원등록하고 "master"로 설정
+			if(pwd.getText().equals("master")) {
+				MasterMain mm = new MasterMain();
+				//id와 pwd가 맞았을 경우 	
+			    mlm.getFrom();
+			    db.getAlert("Welcome Sir.");
+			    db.closeStage(root);
+				}else db.getAlert("login fail");
+		}else db.getAlert("login fail");
 	}
 	
 	public void masterBack() {	  //마스터 뒤로가기 onAction
-		Stage s = (Stage)root.getScene().getWindow();
-		s.close();
+		db.closeStage(root);
 	}
-	
 }
+
